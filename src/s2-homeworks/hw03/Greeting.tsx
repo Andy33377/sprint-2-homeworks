25d29c2c14efd75d7089b23e4175d7e1a536a3ec
Andy33377 <andy.baltenko@gmail.com>
2026-06-04 21:02:37 +0300

    fix hw3 counter

--- src/s2-homeworks/hw03/Greeting.tsx
+++ src/s2-homeworks/hw03/Greeting.tsx
@@ -27,8 +27,8 @@ const Greeting: React.FC<GreetingPropsType> = ({
 
   return (
     <div id={"hw3-form"} className={s.greetingForm}>
-      <div id="hw3-users-total" className={s.text}>
-        Людей добавили: {totalUsers}
+      <div className={s.text}>
+        Людей добавили: <span id="hw3-users-total">{totalUsers}</span>
       </div>
 
       <div className={s.inputAndButtonContainer}>
