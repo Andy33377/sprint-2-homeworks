ec991d57be364b087a5152c4a19d3c426ad68366
Andy33377 <andy.baltenko@gmail.com>
2026-07-18 22:06:37 +0300

    feat: inplement hw8

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -1,20 +1,33 @@
-import {UserType} from '../HW8'
+import { UserType } from "../HW8";
 
-type ActionType =
-    | { type: 'sort'; payload: 'up' | 'down' }
-    | { type: 'check'; payload: number }
-
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
-    switch (action.type) {
-        case 'sort': { // by name
-
-            return state // need to fix
-        }
-        case 'check': {
-
-            return state // need to fix
-        }
-        default:
-            return state
+export type ActionType =
+  | {
+      type: "sort";
+      payload: "up" | "down";
     }
-}
+  | {
+      type: "check";
+      payload: number;
+    };
+
+export const homeWorkReducer = (
+  state: UserType[],
+  action: ActionType,
+): UserType[] => {
+  switch (action.type) {
+    case "sort": {
+      const sortedPeople = [...state].sort((a, b) =>
+        a.name.localeCompare(b.name, "ru"),
+      );
+
+      return action.payload === "up" ? sortedPeople : sortedPeople.reverse();
+    }
+
+    case "check": {
+      return state.filter((person) => person.age >= action.payload);
+    }
+
+    default:
+      return state;
+  }
+};
